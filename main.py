8c30079d1c389afe20a52484174cd954dfd0ac3d
Eden Marco <emarco177@gmail.com>
2023-11-07 08:50:55 +0200

    added node post processosors

--- main.py
+++ main.py
@@ -1,17 +1,26 @@
 from dotenv import load_dotenv
+from llama_index.chat_engine.types import ChatMode
+from llama_index.indices.postprocessor import SentenceEmbeddingOptimizer
+
+from node_postprocessors.duplicate_postprocessing import (
+    DuplicateRemoverNodePostprocessor,
+)
+
+load_dotenv()
 import os
 
 from llama_index.callbacks import LlamaDebugHandler, CallbackManager
-from llama_index.chat_engine.types import ChatMode
-from llama_index.llms import OpenAI
 
-load_dotenv()
 import streamlit as st
 import pinecone
 from llama_index import VectorStoreIndex, ServiceContext
 from llama_index.vector_stores import PineconeVectorStore
 
 print("***Streamlit LlamaIndex Documentation Helper***")
+
+llama_debug = LlamaDebugHandler(print_trace_on_end=True)
+callback_manager = CallbackManager(handlers=[llama_debug])
+service_context = ServiceContext.from_defaults(callback_manager=callback_manager)
 
 
 @st.cache_resource(show_spinner=False)
@@ -23,10 +32,6 @@ def get_index() -> VectorStoreIndex:
     pinecone_index = pinecone.Index(index_name="llamaindex-documentation-helper")
     vector_store = PineconeVectorStore(pinecone_index=pinecone_index)
 
-    llama_debug = LlamaDebugHandler(print_trace_on_end=True)
-    callback_manager = CallbackManager(handlers=[llama_debug])
-    service_context = ServiceContext.from_defaults(callback_manager=callback_manager)
-
     return VectorStoreIndex.from_vector_store(
         vector_store=vector_store, service_context=service_context
     )
@@ -34,8 +39,16 @@ def get_index() -> VectorStoreIndex:
 
 index = get_index()
 if "chat_engine" not in st.session_state.keys():
+    postprocessor = SentenceEmbeddingOptimizer(
+        embed_model=service_context.embed_model,
+        percentile_cutoff=0.5,
+        threshold_cutoff=0.7,
+    )
+
     st.session_state.chat_engine = index.as_chat_engine(
-        chat_mode=ChatMode.CONTEXT, verbose=True
+        chat_mode=ChatMode.CONTEXT,
+        verbose=True,
+        node_postprocessors=[postprocessor, DuplicateRemoverNodePostprocessor()],
     )
 
 
@@ -70,5 +83,10 @@ if st.session_state.messages[-1]["role"] != "assistant":
         with st.spinner("Thinking..."):
             response = st.session_state.chat_engine.chat(message=prompt)
             st.write(response.response)
+            nodes = [node for node in response.source_nodes]
+            for col, node, i in zip(st.columns(len(nodes)), nodes, range(len(nodes))):
+                with col:
+                    st.header(f"Source Node {i+1}: score= {node.score}")
+                    st.write(node.text)
             message = {"role": "assistant", "content": response.response}
             st.session_state.messages.append(message)
---
+++ node_postprocessors/duplicate_postprocessing.py
@@ -0,0 +1,28 @@
+from abc import abstractmethod
+from typing import List, Optional
+
+from llama_index import QueryBundle
+from llama_index.schema import NodeWithScore
+
+
+class DuplicateRemoverNodePostprocessor:
+    """Node postprocessor."""
+
+    @abstractmethod
+    def postprocess_nodes(
+        self, nodes: List[NodeWithScore], query_bundle: Optional[QueryBundle]
+    ) -> List[NodeWithScore]:
+        """Postprocess nodes."""
+        print("_postprocess_nodes enter")
+
+        unique_hashes = set()
+        unique_nodes = []
+
+        for node in nodes:
+            node_hash = node.node.hash
+
+            if node_hash not in unique_hashes:
+                unique_hashes.add(node_hash)
+                unique_nodes.append(node)
+
+        return unique_nodes
